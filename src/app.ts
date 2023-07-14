3339495f1f4ad57659d00c6418399004683fd219
imam-hossain <zunayedemon6t9@gmail.com>
2023-07-15 02:26:52 +0600

    create app.ts file\

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import cookieParser from 'cookie-parser';
+import { Application } from 'express';
+import express from 'express'
+import cors from 'cors'
+
+const app: Application = express();
+
+app.use(cors());
+app.use(cookieParser());
+
+//parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.use('/api/v1', routes);
+
+export default app;
